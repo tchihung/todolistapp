08d365e776f366426aaf1e026c0f96df118c0006
hungt.fe <tran.hung@hitachivantara.com>
2022-07-21 17:59:31 +0700

    thu 5

---
+++ src/redux/reducer.js
@@ -0,0 +1,28 @@
+const initState={
+    filters: {
+        search: '',
+        status: 'All',
+        priority: [],
+    },
+    todoList: [
+        {id: 1, name: 'python', completed: false, priority: 'Medium'},
+        {id: 2, name: 'Java', completed: true, priority: 'High'},
+        {id: 3, name: 'Golang', completed: false, priority: 'Low'},
+    ]
+}
+
+const rootReducer = (state=initState,action)=>{
+    switch (action.type) {
+        case 'todoList/addTodo':
+            return {
+                ...state,
+                todoList: [
+                    ...state.todoList,
+                    {id: 4, name: 'C++', completed: false, priority: 'Medium'},
+                ]
+            }
+        default:
+            return state;
+    }
+}
+export default rootReducer;
