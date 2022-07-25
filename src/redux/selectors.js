09400950d9e847bd2c4fc0310387e1381c158d4b
hungt.fe <tran.hung@hitachivantara.com>
2022-07-25 18:08:31 +0700

    mon

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,9 +1,18 @@
 import { createSelector } from "reselect";
 
-export const todoListSelectors= (state) => {
-    const todoRemaining =state.todoList.filter((todo)=>{
-        return todo.name.includes(state.filters.search);
+export const searchTextSelector= (state)=> state.filter.search;
+export const todoListSelector= (state)=> state.todoList;
+
+
+// export const todoListSelectors= (state) => {
+//     const todoRemaining =state.todoList.filter((todo)=>{
+//         return todo.name.includes(state.filters.search);
+//     })
+//     return todoRemaining;
+// }
+// export const searchTextSelector=(state) => state.filters.search;
+export const todosRemainingSelector = createSelector(todoListSelector,searchTextSelector,(todoList, searchText) => {
+    return todoList.filter((todo)=>{
+        return todo.name.includes(searchText);
     })
-    return todoRemaining;
-}
-export const searchTextSelector=(state) => state.filters.search;
+})
