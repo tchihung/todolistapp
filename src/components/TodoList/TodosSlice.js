9801a20faaee87eadc1863ab4558291b354d3e4d
tchihung <tranchihung.2000@gmail.com>
2022-08-01 04:47:30 +0700

    e

--- src/components/TodoList/TodosSlice.js
+++ src/components/TodoList/TodosSlice.js
@@ -1,18 +1,15 @@
-const initState= [
-    {id: 1, name: 'python', completed: false, priority: 'Medium'},
-    {id: 2, name: 'Java', completed: true, priority: 'High'},
-    {id: 3, name: 'Golang', completed: false, priority: 'Low'},
-]
+const nitStaite = [
+  { id: 1, name: "python", completed: false, priority: "Medium" },
+  { id: 2, name: "Java", completed: true, priority: "High" },
+  { id: 3, name: "Golang", completed: false, priority: "Low" },
+];
 
-const todoListReducer = (state=initState,action)=>{
-    switch (action.type) {
-        case 'todoList/addTodo':
-            return [
-                    ...state,
-                    action.payload
-                ]
-        default:
-            return state;
-    }
-}
+const todoListReducer = (state = nitStaite, action) => {
+  switch (action.type) {
+    case "todoList/addTodo":
+      return [...state.todoList, action.payload];
+    default:
+      return state;
+  }
+};
 export default todoListReducer;
